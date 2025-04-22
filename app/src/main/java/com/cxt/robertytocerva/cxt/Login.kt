ee38ccd8677428fbcba09d20f9cee7d63e8de513
robertytocerva <robertytocerva2004@gmail.com>
2025-04-22 15:23:40 -0600

    carga inicial del proyecto

---
+++ app/src/main/java/com/cxt/robertytocerva/cxt/Login.kt
@@ -0,0 +1,49 @@
+package com.cxt.robertytocerva.cxt
+
+import android.graphics.drawable.TransitionDrawable
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class Login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_login)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+        gradientAnimation()
+
+    }
+
+    fun gradientAnimation() {
+        val layout = findViewById<ConstraintLayout>(R.id.main)
+        val transition = layout.background as? TransitionDrawable
+
+        var forward = true
+        val handler = Handler(Looper.getMainLooper())
+        val duration = 3000L // duración de cada transición
+
+        val runnable = object : java.lang.Runnable {
+            override fun run() {
+                if (forward) {
+                    transition?.startTransition(duration.toInt())
+                } else {
+                    transition?.reverseTransition(duration.toInt())
+                }
+                forward = !forward
+                handler.postDelayed(this, duration)
+            }
+        }
+
+        handler.post(runnable)
+    }
+}
---
+++ app/src/main/java/com/cxt/robertytocerva/cxt/pantallaCarga.kt
@@ -0,0 +1,59 @@
+package com.cxt.robertytocerva.cxt
+
+import android.graphics.drawable.TransitionDrawable
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import android.content.Intent
+import kotlinx.coroutines.*
+class pantallaCarga : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_pantalla_carga)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        gradientAnimation()
+        GlobalScope.launch {
+            delay(3000) // 2 segundos
+            withContext(Dispatchers.Main) {
+                startActivity(Intent(this@pantallaCarga, Login::class.java))
+                finish() // Evita que regresen con el botón atrás
+            }
+        }
+    }
+
+    fun gradientAnimation() {
+        val layout = findViewById<ConstraintLayout>(R.id.main)
+        val transition = layout.background as? TransitionDrawable
+
+        var forward = true
+        val handler = Handler(Looper.getMainLooper())
+        val duration = 3000L // duración de cada transición
+
+        val runnable = object : java.lang.Runnable {
+            override fun run() {
+                if (forward) {
+                    transition?.startTransition(duration.toInt())
+                } else {
+                    transition?.reverseTransition(duration.toInt())
+                }
+                forward = !forward
+                handler.postDelayed(this, duration)
+            }
+        }
+
+        handler.post(runnable)
+    }
+}
