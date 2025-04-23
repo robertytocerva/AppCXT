bc4aa9d0cbdca75e73632a6d99731160c997f712
robertytocerva <robertytocerva2004@gmail.com>
2025-04-22 23:41:09 -0600

    activity_registro.xml completa, solo faltan detalles esteticos. Además se configurón sombras

--- app/src/main/java/com/cxt/robertytocerva/cxt/Registro.kt
+++ app/src/main/java/com/cxt/robertytocerva/cxt/Registro.kt
@@ -1,8 +1,12 @@
 package com.cxt.robertytocerva.cxt
 
+import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -16,5 +20,29 @@ class Registro : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        gradientAnimation()
+    }
+
+    fun gradientAnimation() {
+        val layout = findViewById<ConstraintLayout>(R.id.main)
+        val transition = layout.background as? TransitionDrawable
+
+        var forward = true
+        val handler = Handler(Looper.getMainLooper())
+        val duration = 3000L // duración de cada transición
+
+        val runnable = object : java.lang.Runnable {
+            override fun run() {
+                if (forward) {
+                    transition?.startTransition(duration.toInt())
+                } else {
+                    transition?.reverseTransition(duration.toInt())
+                }
+                forward = !forward
+                handler.postDelayed(this, duration)
+            }
+        }
+
+        handler.post(runnable)
     }
 }
