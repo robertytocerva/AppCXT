daae2b581ecf1a5c2e2db86d90a28040f7951619
robertytocerva <robertytocerva2004@gmail.com>
2025-04-23 22:51:01 -0600

    Implementación de la animación de gradiente en la actividad RegistroNino.kt. Se añade lógica para cambiar los gradientes de fondo.

--- app/src/main/java/com/cxt/robertytocerva/cxt/RegistroNino.kt
+++ app/src/main/java/com/cxt/robertytocerva/cxt/RegistroNino.kt
@@ -1,13 +1,17 @@
 package com.cxt.robertytocerva.cxt
 
 import android.content.Intent
+import android.graphics.drawable.TransitionDrawable
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Spinner
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
@@ -65,6 +69,29 @@ class RegistroNino : AppCompatActivity() {
         }
         //fin listeners
 
+        gradientAnimation()
+    }
 
+    fun gradientAnimation() {
+        val layout = findViewById<ConstraintLayout>(R.id.main)
+        val transition = layout.background as? TransitionDrawable
+
+        var forward = true
+        val handler = Handler(Looper.getMainLooper())
+        val duration = 3000L // duración de cada transición
+
+        val runnable = object : java.lang.Runnable {
+            override fun run() {
+                if (forward) {
+                    transition?.startTransition(duration.toInt())
+                } else {
+                    transition?.reverseTransition(duration.toInt())
+                }
+                forward = !forward
+                handler.postDelayed(this, duration)
+            }
+        }
+
+        handler.post(runnable)
     }
 }
